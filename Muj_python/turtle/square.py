c8e430f7dd79c5c7687c4bd81032ef90a52c2712
rostislavklimes <rostislavklimes@hotmail.com>
2026-05-28 22:20:01 +0200

    Added square

---
+++ Muj_python/turtle/square.py
@@ -0,0 +1,21 @@
+import turtle
+import time
+
+
+zelva = turtle.Turtle("square")
+window = turtle.setup(600, 600)
+window = turtle.bgcolor(244/255, 255/255, 28/255)
+
+zelva.penup()
+zelva.forward(50)
+zelva.left(90)
+zelva.pendown()
+zelva.forward(50)
+time.sleep(0.5)
+for i in range(4):
+    zelva.left(90)
+    zelva.forward(100)
+    time.sleep(0.5)
+
+
+turtle.done()
